adf96227c75d97bd6c9305251ff4ec6bd004ff3a
Evgeny <98ba17E1686!@>
2023-08-10 09:08:24 +0700

    first

---
+++ src/test/java/ru/kazim/spring/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kazim.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
