36bc44bb4f8ad8b10f046496e09e21354ef51c62
Evgeny <98ba17E1686!@>
2023-08-10 09:23:01 +0700

    first

--- src/main/java/ru/kazim/spring/AppApplication.java
+++ src/main/java/ru/kazim/spring/AppApplication.java
@@ -7,27 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(AppApplication.class, args);
-
-        int[] a = new int[]{1, 2, 4, 5};
-        int[] b = new int[]{3, 3, 4};
-        int[] c = new int[]{2, 3, 4, 5, 6};
-
-        System.out.println(find(a, b, c));
-    }
-
-    static int find(int[] a, int[] b, int[] c) {
-        for (int i : a) {
-            for (int j : b) {
-                if (i == j) {
-                    for (int k : c) {
-                        if (j == k) {
-                            return i;
-                        }
-                    }
-                }
-            }
-        }
-        return -1;
+        SpringApplication.run(AppApplication.class, args);
     }
 }
